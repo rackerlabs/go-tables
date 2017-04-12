1544e52ba6fd6d55ca3dcf3fb5197f4589013d3e
Jim Rosser <jarosser06@gmail.com>
2017-04-12 08:59:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Go Tables
+---------
+Generate pretty tables as strings.
+
+Ex:
+```
+|InstanceID         |Name                      |Platform Type|Ping Status|Agent Version|Latest|
+------------------------------------------------------------------------------------------------
+|i-234lk2j34        |MyFirstInstance           |Linux        |Online     |2.0.716.0    |false |
+|i-234lkjlkl        |Campus                    |Linux        |Online     |2.0.730.0    |true  |
+|i-04e9ba2ce21710d01|Ed-TPA                    |Linux        |Online     |2.0.730.0    |true  |
+|i-1625c98ze6bb84720|This-is-a-really-long-name|Linux        |Online     |2.0.730.0    |true  |
+```
---
+++ tables/table_test.go
@@ -0,0 +1,59 @@
+package tables
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTable(t *testing.T) {
+	row1 := []string{"foo", "foobar"}
+	row2 := []string{"bar", "foo"}
+	sampleData := [][]string{row1, row2}
+
+	expect := `|foo|foobar|
+-------------
+|bar|foo   |
+`
+
+	got := Table(sampleData, true)
+	if got != expect {
+		t.Errorf("Expected:\n %s, Got:\n %s", expect, got)
+	}
+}
+
+func TestTable_withoutHeader(t *testing.T) {
+	row1 := []string{"foo", "foobar"}
+	row2 := []string{"bar", "foo"}
+	sampleData := [][]string{row1, row2}
+
+	expect := `|foo|foobar|
+|bar|foo   |
+`
+
+	got := Table(sampleData, false)
+	if got != expect {
+		t.Errorf("Expected:\n %s, Got:\n %s", expect, got)
+	}
+}
+
+func TestExtractColumn(t *testing.T) {
+	row1 := []string{"foo", "foobar"}
+	row2 := []string{"bar", "barfoo"}
+	sampleData := [][]string{row1, row2}
+
+	res := extractColumn(sampleData, 0)
+	expected := []string{"foo", "bar"}
+
+	if !reflect.DeepEqual(res, expected) {
+		t.Errorf("Expected %+v, Got %+v", expected, res)
+	}
+}
+
+func TestColumnWidth(t *testing.T) {
+	column := []string{"foo", "barbarbar", "foobar"}
+
+	res := columnWidth(column)
+	if res != 9 {
+		t.Errorf("Expected column width to be 9")
+	}
+}
